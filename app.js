61cb764c9fcf8b5641e8719ebb8978329b9cc6cb
Fearmirage <giacomobalugani@gmail.com>
2026-04-10 15:48:13 +0200

    feat (backend): add form submission handling

--- app.js
+++ app.js
@@ -25,3 +25,17 @@ fetch('prompts.json')
     .catch(error => {
         console.error('Error loading JSON:', error);
     });
+
+form.addEventListener('submit', function (event) {
+  event.preventDefault();
+
+  const NewPrompt = {
+    title: document.getElementById('title').value,
+    category: document.getElementById('category').value,
+    prompt: document.getElementById('content').value
+  };
+
+  AddPromptToList(NewPrompt);
+
+  form.reset();
+});
